dae1e8eec66530fb847f60102031f2bc22a309ef
Harikrishna Bhadiyadra <bhadiyadraharikrishna@gmail.com>
2024-12-29 10:06:18 +0530

    Adding test for borrowing book

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -8,4 +8,12 @@ public class LibraryTest {
         library.addBook("123", "The Great Gatsby", "F. Scott Fitzgerald", 1925);
         assertTrue(library.isBookAvailable("123"));
     }
+    @Test
+    public void testBorrowBook() {
+        Library library = new Library();
+        library.addBook("123", "The Great Gatsby", "F. Scott Fitzgerald", 1925);
+        library.borrowBook("123");
+        assertFalse(library.isBookAvailable("123"));
+    }
+
 }
