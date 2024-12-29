2f1e01f6e49904d71c0afb1e75c7bd04e614ebb7
Harikrishna Bhadiyadra <bhadiyadraharikrishna@gmail.com>
2024-12-29 10:19:08 +0530

    added test for returning book

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -15,5 +15,12 @@ public class LibraryTest {
         library.borrowBook("123");
         assertFalse(library.isBookAvailable("123"));
     }
-
+    @Test
+    public void testReturnBook() {
+        Library library = new Library();
+        library.addBook("123", "The Great Gatsby", "F. Scott Fitzgerald", 1925);
+        library.borrowBook("123");
+        library.returnBook("123");
+        assertTrue(library.isBookAvailable("123"));
+    }
 }
